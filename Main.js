9620afc02de2195a53a09f68ed5c7f017914a122
Julio Perez <71227818+ajulioperez@users.noreply.github.com>
2022-11-16 22:24:32 -0600

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,16 @@
+function intervals(){
+    
+    timer = setInterval( function() {
+        document.querySelector("#timeinfo").innerHTML = new Date().toString();
+         
+        
+        document.querySelector(".item1").style.backgroundColor = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+        document.querySelector(".item2").style.backgroundColor = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+        document.querySelector(".item3").style.backgroundColor = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+        document.querySelector(".item4").style.backgroundColor = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+        document.querySelector(".item5").style.backgroundColor = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+        // .css('backgroundColor', randomColor);
+        
+    }, 1000);
+}
+
